6edb248af153f9a754b65f0179efbb99bb563b37
BrindhaAnandhan <brindhaorbindu@gmail.com>
2024-01-11 12:41:40 +0530

    Added Dynamic functonality and created backend to stored the data.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class SignUp(models.Model):
+    UserName = models.CharField(max_length = 100, primary_key = True)
+    Email = models.EmailField()
+    Password = models.CharField(max_length = 10)
+    CPassword = models.CharField(max_length = 10)
+    
+    def __str__(self) -> str:
+        return self.UserName
--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def japan(request):
-    return render(request, 'assigment.html')
+    data = {'Invalid': '', 'Valid': ''}
+    if request.method == "POST":
+        UserName = request.POST['user']
+        Email = request.POST['email']
+        Password = request.POST['pass']
+        CPassword = request.POST['repass']
+
+        if Password == CPassword:
+            SU = SignUp.objects.get_or_create(UserName = UserName, Email = Email, Password = Password, CPassword = CPassword)[0]
+            SU.save()
+            data['Valid'] = "Register Successfully"
+        else:
+            data['Invalid'] = "Password incorrect"
+
+    return render(request, 'assigment.html', data)
